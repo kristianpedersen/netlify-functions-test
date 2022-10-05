4df8d4cb32ae57b9476020e85f5db17c678c6ac7
Kristian Pedersen <furkrp@furst.no>
2022-10-05 14:34:37 +0200

    netlify-eksempel

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,17 +1,69 @@
+import algoliasearch from 'algoliasearch'
+import sanityClient, { SanityDocumentStub } from '@sanity/client'
+import indexer from 'sanity-algolia'
+
+const algolia = algoliasearch(
+  'C26QC41PWH',
+  "e23b64dadd4c26f8678c15a2593521fa"
+);
+
+const sanity = sanityClient({
+  projectId: 'sukats6f',
+  dataset: 'test',
+  apiVersion: '2022-09-25',
+  useCdn: false,
+});
+
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+const handler = (req, res) => {
+  if (req.headers['content-type'] !== 'application/json') {
+    res.status(400)
+    res.json({ message: 'Bad request' })
+    return
   }
+
+  const algoliaIndex = algolia.initIndex('my-index');
+
+  const sanityAlgolia = indexer(
+    {
+      post: {
+        index: algoliaIndex,
+        projection: `{
+          title,
+          "path": slug.current,
+          "body": pt::text(body)
+        }`,
+      },
+      article: {
+        index: algoliaIndex,
+        projection: `{
+          heading,
+          "body": pt::text(body),
+          "authorNames": authors[]->name
+        }`,
+      },
+    },
+
+    (document) => {
+      switch (document._type) {
+        case 'post':
+          return Object.assign({}, document, {
+            custom: 'An additional custom field for posts, perhaps?',
+          })
+        case 'article':
+          return {
+            title: document.heading,
+            body: document.body,
+            authorNames: document.authorNames,
+          }
+        default:
+          return document
+      }
+    }
+  )
+
+  return sanityAlgolia.webhookSync(sanity, req.body)
+    .then(() => res.status(200).send("ok"))
 }
 
 module.exports = { handler }
